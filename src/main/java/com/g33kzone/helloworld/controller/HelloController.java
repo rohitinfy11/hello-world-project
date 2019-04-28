81f1ca1ce93b704dfe4f1a224d8352d7dc1ff86c
Babita Gurjar <babitagujar@gmail.com>
2019-04-28 15:44:30 +0530

    testcases added

--- src/main/java/com/g33kzone/helloworld/controller/HelloController.java
+++ src/main/java/com/g33kzone/helloworld/controller/HelloController.java
@@ -5,6 +5,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import com.g33kzone.helloworld.model.Employee;
 
 @RestController
 public class HelloController {
@@ -14,4 +17,16 @@ public class HelloController {
 		
 		return ResponseEntity.status(HttpStatus.OK).body("Hello World - Welcome to GCP Cloud Build using K8s...");
 	}
+	@RequestMapping(value = "/employee", method = RequestMethod.GET)
+	public Employee firstPage() {
+
+		Employee emp = new Employee();
+		emp.setName("emp1");
+		emp.setDesignation("manager");
+		emp.setEmpId("1");
+		emp.setSalary(3000);
+
+		return emp;
+	}
+
 }
